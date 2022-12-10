d4fabc140605df47b3914fa43e5be2b7cb5d69f1
Samuel Alaniz <samuelalaniz@Samuels-MacBook-Air.local>
2022-12-10 14:28:28 -0600

    for loop examples

---
+++ python-examples/for-loops.py
@@ -0,0 +1,21 @@
+
+print('simple for loop')
+for i in range(10):
+    print(i)
+
+print('matrix for loop - code golf')
+num_cols = 3
+num_rows = 2
+matrix = [[' '] * num_cols for _ in range(num_rows)]
+print(matrix)
+
+print('matrix for loop - easy')
+cols = []
+for _ in range(num_cols):
+    cols = cols + ['']
+
+rows = []
+for _ in range(num_rows):
+    rows.append(cols)
+
+print(rows)
