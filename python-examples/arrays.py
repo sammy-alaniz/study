cc456b217c21608aa4439d1ee94c575bed92dfa4
Samuel Alaniz <samuelalaniz@Samuels-Air.attlocal.net>
2022-12-13 09:11:13 -0600

    reiviewed arrays

--- python-examples/arrays.py
+++ python-examples/arrays.py
@@ -14,10 +14,22 @@ def sort_array_with_lambda():
     array.sort(key= lambda x: x[0])
     print(array)
 
+def insert_array():
+    array = [[0,30],[30,5],[15,20]]
+    print('before insertion ' + str(array))
+    array.insert(2,'a')
+    print('after insertion ' + str(array))
 
-    
+import array as arr
+
+def array_import():
+    c = arr.array('i',[1,2,3,4,5,6,7,8,9])
+    print('Element you have accessed', c[2])
+    print('Element you have accesssed:', c[1])
 
 if __name__ == "__main__":
     #delete_array_element()
-    sort_array_with_lambda()
+    #sort_array_with_lambda()
+    insert_array()
+    array_import()
 
