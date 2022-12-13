905c7dcac87e88ee2c3c9e21713a066606c50a39
Samuel Alaniz <samuelalaniz@Samuels-Air.attlocal.net>
2022-12-12 23:15:13 -0600

    start on finding leaves of a binary tree, seems like i need to use recursion more

--- python-examples/dictionary.py
+++ python-examples/dictionary.py
@@ -7,3 +7,18 @@ item = "sammy"
 
 if item in d:
     print("contains item")
+
+
+from collections import defaultdict
+
+def def_value():
+    #return "not present"
+    return list
+
+d = defaultdict(def_value)
+d['a'] = 1
+d['b'] = 2
+
+print(d['a'])
+print(d['b'])
+print(d['c'])
